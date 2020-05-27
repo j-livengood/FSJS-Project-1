d749979d23ce3f3852d031701df29a1053c5750f
Joram Livengood <joram.livengood@gmail.com>
2020-05-27 17:00:04 -0500

    Added quotes array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -10,8 +10,43 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+let quotes = [
+  {
+    quote: "Frankly, my dear, I don't give a damn.",
+    character: 'Rhett Butler',
+    actor: 'Clark Gable',
+    movie: 'Gone With the Wind',
+    year: 1939
+  },
+  {
+    quote: "I'm going to make him an offer he can't refuse.",
+    character: 'Don Vito Corleone',
+    actor: 'Marlon Brando',
+    movie: 'The Godfather',
+    year: 1972
+  },
+  {
+    quote: "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
+    character: 'Terry Malloy',
+    actor: 'Marlon Brando',
+    movie: 'On the Waterfront',
+    year: 1954
+  },
+  {
+    quote: "Toto, I've got a feeling we're not in Kansas anymore.",
+    character: 'Dorothy',
+    actor: 'Judy Garland',
+    movie: 'The Wizard of Oz',
+    year: 1939
+  },
+  {
+    quote: "Here's looking at you, kid.",
+    character: 'Rick Blaine',
+    actor: 'Humphrey Bogart',
+    movie: 'Casablanca',
+    year: 1942
+  }
+];
 
 /***
  * `getRandomQuote` function
